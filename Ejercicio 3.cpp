fceee94137dd03a1733ca7b53174618c475bc4cd
Armenteros05 <sarmerod@myuax.com>
2023-11-16 12:43:50 +0100

    ejercicio 3

--- Ejercicio 3.cpp
+++ Ejercicio 3.cpp
@@ -1,20 +1,40 @@
 #include <iostream>
-using namespace std;
-namespace Geometria{
-    class circulo
-{
+#include <cmath>
 
+namespace Geometria {
+    class Circulo {
     private:
-    double radio;
+        double radio;
+
     public:
-    circulo(double r);
-    double area();
-    double perimetro();
+        Circulo(double r) : radio(r) {}
 
-};
+        double calcularArea() const {
+            return M_PI * radio * radio;
+        }
 
+        double calcularPerimetro() const {
+            return 2 * M_PI * radio;
+        }
 
+        void imprimirResultados() const {
+            std::cout << "Perimetro del circulo: " << calcularPerimetro() << std::endl;
+            std::cout << "Pulsa una tecla para continuar..." << std::endl;
+            std::cin.get(); // Espera a que el usuario presione una tecla
+            std::cout << "Area del circulo: " << calcularArea() << std::endl;
+            std::cout << "Pulsa una tecla para salir..." << std::endl;
+            std::cin.get(); // Espera a que el usuario presione una tecla
+        }
+    };
 }
 
+int main() {
+    double radio;
+    std::cout << "Ingrese el radio del circulo: ";
+    std::cin >> radio;
 
+    Geometria::Circulo circulo(radio);
+    circulo.imprimirResultados();
 
+    return 0;
+}
